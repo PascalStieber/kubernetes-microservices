22142199a11b2455250a931dca30acc2fb66bd9f
Pascal Stieber <pascalstieber@Pascals-MBP.fritz.box>
2019-03-16 21:05:27 +0100

    just documented mount issues in minikube in cheatsheet.sh

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -1,3 +1,10 @@
-cheatsheet
+#cheatsheet
 
-kubectl exec -ti mongodb-67697fcf45-68s9z -- bash
+kubectl exec -ti [podname] -- bash
+
+# persistentvolumes:
+# ist der typ 'hostpath', werden die directories nicht automatisch auf dem host-system, sondern erst einmal nur in minikube angelegt.
+# anmelden in minikube mit: minikube ssh 
+# dann in das unter 'hostpath' angegebene verzeichnis wechseln. dort sind die dateien abgelegt.
+# will man das verzeichnis auf dem hostsystem (bare metal) angelegt bekommen, muss das verzeichnis in minikube gemountet werden:
+# minikube mount HOST_MOUNT_DIRECTORY:VM_MOUNT_DIRECTORY(ex:"/host-home:/vm-home")
